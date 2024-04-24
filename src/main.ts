f18b829f52c1809dcb87b4766b54f7bf8e4111e3
toaonly <toaonly42@gmail.com>
2024-04-25 00:38:40 +0900

    init

---
+++ src/main.ts
@@ -0,0 +1,28 @@
+import './style.css'
+import {
+  getKeys,
+  getValueByKey,
+  transformUppercaseKey,
+} from '@toaonly/fc-package-ch05-clip05'
+
+const user = {
+  name: '홍길동',
+  age: 20,
+  gender: 'M',
+  job: 'college student',
+  car: ['Car_01', 'Car_02'],
+}
+
+console.log(getKeys(user))
+
+console.log(getValueByKey(user, 'name'))
+console.log(getValueByKey(user, 'age'))
+console.log(getValueByKey(user, 'gender'))
+console.log(getValueByKey(user, 'job'))
+console.log(getValueByKey(user, 'car'))
+
+console.log(transformUppercaseKey(user).AGE)
+console.log(transformUppercaseKey(user).CAR)
+console.log(transformUppercaseKey(user).GENDER)
+console.log(transformUppercaseKey(user).JOB)
+console.log(transformUppercaseKey(user).NAME)
