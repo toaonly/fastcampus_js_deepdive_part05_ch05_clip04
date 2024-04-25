8c89edd2bb2406bbd710c16cf82509800de56e34
toaonly <toaonly42@gmail.com>
2024-04-26 00:03:14 +0900

    init

---
+++ src/ex1.ts
@@ -0,0 +1,27 @@
+import {
+  getKeys,
+  getValueByKey,
+  transformUppercaseKey,
+} from 'toaonly_fc_package_ch05_clip05'
+
+const user = {
+  name: '홍길동',
+  age: 20,
+  gender: 'M',
+  job: 'college student',
+  car: ['Car_01', 'Car_02'],
+}
+
+console.log(getKeys(user))
+
+console.log(getValueByKey(user, 'name'))
+console.log(getValueByKey(user, 'age'))
+console.log(getValueByKey(user, 'gender'))
+console.log(getValueByKey(user, 'job'))
+console.log(getValueByKey(user, 'car'))
+
+console.log(transformUppercaseKey(user).AGE)
+console.log(transformUppercaseKey(user).CAR)
+console.log(transformUppercaseKey(user).GENDER)
+console.log(transformUppercaseKey(user).JOB)
+console.log(transformUppercaseKey(user).NAME)
